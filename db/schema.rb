4b708175de847239d45e3f45ba14b9972487fa31
Michał Orynicz <morynicz@gmail.com>
2015-07-25 02:43:44 +0200

    Changed underlying db to postgress
    Heroku needs postgress

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150724201649) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "players", force: :cascade do |t|
     t.string   "name"
     t.string   "surname"
