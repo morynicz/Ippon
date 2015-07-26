9e9cbfab08dc1b3bce3a0d04957013c1de04464b
Michał Orynicz <morynicz@gmail.com>
2015-07-26 21:01:14 +0200

    Fix FinalFight model typo

--- app/models/final_fight.rb
+++ app/models/final_fight.rb
@@ -1,6 +1,6 @@
 class FinalFight < ActiveRecord::Base
   has_one :fight
   belongs_to :tournament
-  has_one :previous_aka_fight, class 'FightFight'
-  has_one :previous_shiro_fight, class 'FinalFight' 
+  has_one :previous_aka_fight, class 'FinalFight'
+  has_one :previous_shiro_fight, class 'FinalFight'
 end
