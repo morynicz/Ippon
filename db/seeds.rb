917bed92638c61d7a1074e6cb2c2537745850c4e
Michał Orynicz <morynicz@gmail.com>
2015-07-26 22:31:23 +0200

    Seed up the ranks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#b1 = Book.create(title: "American Sniper", author: "Chris Kyle, Scott McEwen, Jim DeFelice", description: "A memoir about battlefield experiences in Iraq by the Navy SEALs sniper.", publisher: "Morrow/HarperCollins", weeks_on_list: 63, rank_this_week: 1)
+Rank.create(name: '5 Kyu', is_dan: false)
+Rank.create(name: '4 Kyu', is_dan: false)
+Rank.create(name: '3 Kyu', is_dan: false)
+Rank.create(name: '2 Kyu', is_dan: false)
+Rank.create(name: '1 Kyu', is_dan: false)
+Rank.create(name: '1 Dan', is_dan: true)
+Rank.create(name: '2 Dan', is_dan: true)
+Rank.create(name: '3 Dan', is_dan: true)
+Rank.create(name: '4 Dan', is_dan: true)
+Rank.create(name: '5 Dan', is_dan: true)
+Rank.create(name: '6 Dan', is_dan: true)
+Rank.create(name: '7 Dan', is_dan: true)
+Rank.create(name: '8 Dan', is_dan: true)
