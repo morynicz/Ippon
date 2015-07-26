2427bb25fe50434d227e18b8deeee268988118bc
Michał Orynicz <morynicz@gmail.com>
2015-07-26 22:28:39 +0200

    Updated schema for groups table

--- db/migrate/20150725131410_create_groups.rb
+++ db/migrate/20150725131410_create_groups.rb
@@ -2,6 +2,8 @@ class CreateGroups < ActiveRecord::Migration
   def change
     create_table :groups do |t|
       t.string :name
+      t.references :fight
+      t.references :tournament
       t.timestamps null: false
     end
   end
