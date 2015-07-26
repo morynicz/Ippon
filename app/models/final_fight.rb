d00c415728292102e264080cc4c56bec873bef6d
Michał Orynicz <morynicz@gmail.com>
2015-07-26 21:04:17 +0200

    Another fix for FinalFights model

--- app/models/final_fight.rb
+++ app/models/final_fight.rb
@@ -1,6 +1,6 @@
 class FinalFight < ActiveRecord::Base
   has_one :fight
   belongs_to :tournament
-  has_one :previous_aka_fight, class 'FinalFight'
-  has_one :previous_shiro_fight, class 'FinalFight'
+  has_one :previous_aka_fight, class_name: 'FinalFight'
+  has_one :previous_shiro_fight, class_name: 'FinalFight'
 end
