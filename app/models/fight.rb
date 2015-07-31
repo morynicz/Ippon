86f236fee9b3df50f656c867b746d836ff77f31d
Michał Orynicz <morynicz@gmail.com>
2015-07-31 18:58:48 +0200

    Fix for badly written fights
    Its :class_name , not :class

--- app/models/fight.rb
+++ app/models/fight.rb
@@ -1,7 +1,7 @@
 class Fight < ActiveRecord::Base
   belongs_to :fightable, polymorphic: true
-  belongs_to :shiro, class: 'Player'
-  belongs_to :aka, class: 'Player'
+  belongs_to :shiro, class_name: 'Player'
+  belongs_to :aka, class_name: 'Player'
   belongs_to :tournament, through: :fightable
   belongs_to :fight_state
   has_one :location
