61528fd43cb709525ec4bbb3ebfae27fbb835e4c
Michał Orynicz <morynicz@gmail.com>
2015-07-31 19:16:14 +0200

    Another attempt at fight

--- app/models/fight.rb
+++ app/models/fight.rb
@@ -2,8 +2,6 @@ class Fight < ActiveRecord::Base
   belongs_to :fightable, polymorphic: true
   belongs_to :shiro, class_name: 'Player'
   belongs_to :aka, class_name: 'Player'
-  belongs_to :tournament, through: :fightable
-  belongs_to :fight_state
   has_one :location
   has_one :state
 end
