48fa9a7d012eb1295f68567da66ab9e6c34ca2d1
Michał Orynicz <morynicz@gmail.com>
2015-08-17 13:21:09 +0200

    Edit create group migration for better references
    Instead of using `references` fields use separate migration
    for ading fight and tounament reference

--- db/migrate/20150725131410_create_groups.rb
+++ db/migrate/20150725131410_create_groups.rb
@@ -2,8 +2,6 @@ class CreateGroups < ActiveRecord::Migration
   def change
     create_table :groups do |t|
       t.string :name
-      t.references :fight
-      t.references :tournament
       t.timestamps null: false
     end
   end
