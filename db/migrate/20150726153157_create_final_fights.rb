a40bec90597a1a669b8b7f47d4ebc5a9ba7c6e90
Michał Orynicz <morynicz@gmail.com>
2015-08-17 13:27:07 +0200

    Fix FinalFight creation migration
    Fix the migration to properly use foreign keys, including to itself

--- db/migrate/20150726153157_create_final_fights.rb
+++ db/migrate/20150726153157_create_final_fights.rb
@@ -1,10 +1,10 @@
 class CreateFinalFights < ActiveRecord::Migration
   def change
     create_table :final_fights do |t|
-      t.references :previous_aka_fight, index: true
-      t.references :previous_shiro_fight, index: true
-      t.references :fight
-      t.references :tournament
+      t.belongs_to :previous_aka_fight
+      t.belongs_to :previous_shiro_fight
+      t.references :fight, index: true, foreign_key: true
+      t.references :tournament, index: true, foreign_key: true
       t.timestamps null: false
     end
   end
