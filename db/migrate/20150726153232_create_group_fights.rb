c0b6baecec748b848cc603e90b80b835591111ba
Michał Orynicz <morynicz@gmail.com>
2015-08-17 13:29:09 +0200

    MAke group fight c migration ref fight and tournament

--- db/migrate/20150726153232_create_group_fights.rb
+++ db/migrate/20150726153232_create_group_fights.rb
@@ -1,8 +1,8 @@
 class CreateGroupFights < ActiveRecord::Migration
   def change
     create_table :group_fights do |t|
-      t.references :fight
-      t.references :tournament
+      t.references :fight, index: true, foreign_key: true
+      t.references :tournament, index: true, foreign_key: true
       t.timestamps null: false
     end
   end
