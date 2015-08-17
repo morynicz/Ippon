b476e9b4ee991807889a20af956999061cb6b475
Michał Orynicz <morynicz@gmail.com>
2015-08-17 13:24:22 +0200

    Make fight belong to aka and shiro players

--- db/migrate/20150725133141_create_fights.rb
+++ db/migrate/20150725133141_create_fights.rb
@@ -3,6 +3,8 @@ class CreateFights < ActiveRecord::Migration
     create_table :fights do |t|
       t.integer :aka_points
       t.integer :shiro_points
+      t.belongs_to :aka
+      t.belongs_to :shiro
       t.timestamps null: false
     end
   end
