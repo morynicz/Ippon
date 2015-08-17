7953e0c4432b48a6787a7066f8fab0d2b893695a
Michał Orynicz <morynicz@gmail.com>
2015-08-17 13:25:08 +0200

    MAke location really reference tournament

--- db/migrate/20150726152251_create_locations.rb
+++ db/migrate/20150726152251_create_locations.rb
@@ -2,7 +2,7 @@ class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
       t.string :name
-      t.references :tournament
+      t.references :tournament, index: true, foreign_key: true
       t.timestamps null: false
     end
   end
