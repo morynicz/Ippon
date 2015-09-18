c0ee6f63127aa7008625102286f94e2ca316e3b5
Michał Orynicz <morynicz@gmail.com>
2015-09-18 23:09:41 +0200

    Fix issue with not initialised link in tournament#edit

--- app/assets/javascripts/participation_adder.js
+++ app/assets/javascripts/participation_adder.js
@@ -1,4 +1,7 @@
 
-$(document).on("change","#participant",function(){
+var prep_participant = function(){
   $("#participant_link").attr("href", "/participations/create_new/"+$("#participant :selected").val()+"/"+$("#hi").html());
-});
+};
+
+$(document).ready(prep_participant);
+$(document).on("change","#participant",prep_participant);
