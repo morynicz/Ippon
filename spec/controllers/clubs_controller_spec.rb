ed66220d335f5b36cc5db5de8b7ec71c6fa5e555
Michał Orynicz <morynicz@gmail.com>
2016-05-16 08:11:17 +0200

    ClubsController.rb: add new test cases and log messages

--- spec/controllers/clubs_controller_spec.rb
+++ spec/controllers/clubs_controller_spec.rb
@@ -94,21 +94,22 @@ describe ClubsController do
   end
 
   describe "create" do
-    context "when the user is not authorized" do
+    context "when the user is not authenticated" do
       before do
         prepare_user
         prepare_create
       end
-      puts "#{User.all} "
+      puts "cre un #{User.all} "
       it{expect(response.status).to eq(401)}
     end
 
-    context "when the user is authorized" do
+    context "when the user is authenticated" do
       before do
         prepare_user(true, false)
         prepare_create
       end
 
+      puts "cre au #{User.all} "
       it {expect(response.status).to eq(201)}
       it {expect(Club.last.name).to eq("CreatedClub")}
       it {expect(Club.last.city).to eq("CreativeCity")}
@@ -124,41 +125,62 @@ describe ClubsController do
     let(:update_club_hash) {{
       name: "ClubUpdateYo", city: "FutureCity", description: "So up to date that in the future"
     }}
-    context "when the club exists and user is authorized" do
+    context "when the club exists" do
       let(:club) {
         Club.create!(name: "UpdatedClub", city: "DateUpCity", description: "They are up to date")
       }
 
-      before do
-        prepare_user(true,true,club.id)
-        prepare_update(club.id, update_club_hash)
-        club.reload
+      context "when the user is authorized" do
+        before do
+          prepare_user(true,true,club.id)
+          prepare_update(club.id, update_club_hash)
+          club.reload
+        end
+
+        puts "up au #{User.all} "
+        it{expect(response.status).to eq(204)}
+        it{expect(club.name).to eq("ClubUpdateYo")}
+        it{expect(club.city).to eq("FutureCity")}
+        it{expect(club.description).to eq("So up to date that in the future")}
       end
 
-      it{expect(response.status).to eq(204)}
-      it{expect(club.name).to eq("ClubUpdateYo")}
-      it{expect(club.city).to eq("FutureCity")}
-      it{expect(club.description).to eq("So up to date that in the future")}
+      context "when the user isn't authorized" do
+        before do
+          prepare_user(true,false,club.id)
+          prepare_update(club.id, update_club_hash)
+          club.reload
+        end
+
+        puts "up unau #{User.all} "
+        it{expect(response.status).to eq(500)}
+        it{expect(club.name).to eq("UpdatedClub")}
+        it{expect(club.city).to eq("DateUpCity")}
+        it{expect(club.description).to eq("They are up to date")}
+
+      end
     end
 
-    context "when the club doesn't exist and user is authorized" do
-      before do
-        prepare_user(true,true)
-        prepare_update(club_id,update_club_hash)
-      end
-
+    context "when the club doesn't exist" do
       let(:club_id) {-9999}
-      it{expect(response.status).to eq(404)}
-    end
 
-    context "when user is not authorized" do
-      before do
-        prepare_user(true,false)
-        prepare_update(club_id,update_club_hash)
+      #not possible due to constraint on ClubAdmins
+      # context "when the user is authorized" do
+      #   before do
+      #     prepare_user(true,true, club_id)
+      #     prepare_update(club_id,update_club_hash)
+      #   end
+      #
+      #   it{expect(response.status).to eq(404)}
+      # end
+
+      context "when user is not authorized" do
+        before do
+          prepare_user(true,false)
+          prepare_update(club_id,update_club_hash)
+        end
+        puts "up noc unau #{User.all} "
+        it{expect(response.status).to eq(401)}
       end
-
-      let(:club_id) {-9999}
-      it{expect(response.status).to eq(401)}
     end
   end
 
@@ -167,31 +189,52 @@ describe ClubsController do
   end
 
   describe "destroy" do
-    let(:club_id) {
+    let(:club) {
       Club.create!(name: "End of the line club", city: "City of the end", description: "They are finished")
     }
 
-    context "when the user is authorized" do
+    context "when the club exists" do
 
-      context "when the club exists" do
+      context "when the user is authorized" do
         before do
-          preapre_user(true,true,club_id)
-          prepare_destroy(club_id)
+          prepare_user(true,true,club.id)
+          prepare_destroy(club.id)
         end
-
+        puts "des au #{User.all} "
         it{expect(response.status).to eq(204)}
-        it{expect(Club.find_by_id(club_id)).to be_nil}
+        it{expect(Club.find_by_id(club.id)).to be_nil}
       end
 
-      context "when the club doesn't exist" do
-        let(:club_id) {-9999}
+      context "when the user is not authorized" do
+        before do
+          prepare_user(true,false)
+          prepare_destroy(club.id)
+        end
+        puts "des unau #{User.all} "
+        it{expect(response.status).to eq(500)}
+        it{expect(Club.exists?(club.id)).to be true}
+      end
+    end
 
+    context "when the club doesn't exist" do
+      let(:club_id) {-9999}
+      ## Not possible due to ClubAdmins constraints
+      # context "when the user is authorized" do
+      #   before do
+      #     prepare_user(true,true,club_id)
+      #     prepare_destroy(club_id)
+      #   end
+      #
+      #   it{expect(response.status).to eq(404)}
+      # end
+
+      context "when the user is not authorized" do
         before do
           prepare_user(true,false)
           prepare_destroy(club_id)
         end
 
-        it{expect(response.status).to eq(404)}
+        it{expect(response.status).to eq(500)}
       end
     end
   end
