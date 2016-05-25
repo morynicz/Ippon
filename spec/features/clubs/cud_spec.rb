5cd1fa3b01d49223fd56f980018f9f1d8aa086a3
Michał Orynicz <morynicz@gmail.com>
2016-05-25 08:35:14 +0200

    Make CRUD feature test pass authorization

--- spec/features/clubs/cud_spec.rb
+++ spec/features/clubs/cud_spec.rb
@@ -1,15 +1,23 @@
 require 'spec_helper.rb'
 
 feature 'Creating editing and deleting a club', js: true do
-  scenario "CRUD a club" do
+  scenario "CRUD a club when user exists" do
 
     Cl = Struct.new("Cl", :name, :city, :description)
     cl1 = Cl.new("Nuclub", "Newville", "Some description")
     cl2 = Cl.new("UpClub", "Upcity", "Other desc")
+    user = User.create!(username: "usr", email: "mail@mail.org", password: "passpasswd")
 
-    visit '#/clubs/'
+    visit '#/login'
 
-    click_on "New Club"
+    fill_in "email", with: user.email
+    fill_in "password", with: user.password
+
+    click_on "Log In"
+
+    visit "#/clubs/"
+
+    click_on "New Club..."
 
     fill_in "name", with: cl1.name
     fill_in "city", with: cl1.city
@@ -33,11 +41,10 @@ feature 'Creating editing and deleting a club', js: true do
     expect(page).to have_content(cl2.city)
     expect(page).to have_content(cl2.description)
 
-    visit '#/clubs/'
+    visit "#/clubs/"
 
     click_on cl2.name
-
-    click_on "Delete"
+    click_on "delete-club"
 
     expect(Club.find_by_name(cl2.name)).to be_nil
   end
