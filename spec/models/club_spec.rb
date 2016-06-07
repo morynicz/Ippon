dbbad523d69e2cc99f6abf8cc2f8ee07e88501d3
Michał Orynicz <morynicz@gmail.com>
2016-06-07 22:20:29 +0200

    Add tests for club model

--- spec/models/club_spec.rb
+++ spec/models/club_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Club do
-  skip "add some examples to (or delete) #{__FILE__}"
+  let(:creator) {
+    FactoryGirl::create(:user)
+  }
+
+  let(:club_attrs) {
+    FactoryGirl::attributes_for(:club)
+  }
+  describe "Creation with creator specified" do
+    it "Adds creator as an admin after the club is created" do
+      club_attrs["creator"] = creator
+      club = Club.create(club_attrs)
+      expect(ClubAdmin.exists?(club_id: club.id, user_id: creator.id)).to be true
+    end
+  end
+
+  describe "Creation without creator specified" do
+    it "Does not change admn count after the club is created" do
+      expect {
+        Club.create(club_attrs)
+      }.not_to change(ClubAdmin, :count)
+    end
+  end
 end
