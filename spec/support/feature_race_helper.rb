4b4504f9ee9e6db80432a36adadb3fa3cdc9e1b7
Michał Orynicz <morynicz@gmail.com>
2016-06-09 07:28:22 +0200

    Cleanup race helper

--- spec/support/feature_race_helper.rb
+++ spec/support/feature_race_helper.rb
@@ -4,11 +4,6 @@ module FeatureRaceHelpers
     before do |ex|
       if ex.metadata[:raceable]
         has_css?("h1#welcome-title")
-        if has_css?("h1#welcome-title")
-          puts "has"
-        else
-          puts "hasn't"
-        end
       end
     end
   end
