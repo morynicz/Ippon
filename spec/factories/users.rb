4c481fa4f17fcf304b55507143e3502d745ee233
Michał Orynicz <morynicz@gmail.com>
2016-06-09 07:10:13 +0200

    Make sure faked usernames are unique

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,11 @@
 FactoryGirl.define do
+  sequence :uname do |n|
+    "#{Faker::Internet.user_name}#{n}"
+  end
+
   factory :user do
     email { Faker::Internet.email }
-    username {Faker::Internet.user_name}
+    username {generate(:uname)}
     password "password"
     password_confirmation "password"
   end
