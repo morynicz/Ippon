8c44a09c4534fc2ccd430653b01c69a30b78a714
Michał Orynicz <morynicz@gmail.com>
2016-06-11 18:29:05 +0200

    Use factoryGirl in view_spec

--- spec/features/clubs/view_spec.rb
+++ spec/features/clubs/view_spec.rb
@@ -1,39 +1,28 @@
 require 'spec_helper.rb'
 
-cl1name = "New Club"
-cl1city = "Oldville"
-cl1description = "They just arrived"
-
-cl2name = "Old Club"
-cl2city = "Uppercastle"
-cl2description = "Here for around 15 years already"
+cl1attrs = FactoryGirl::attributes_for(:club)
+cl2attrs = FactoryGirl::attributes_for(:club)
 
 feature "Viewing a Club", js: true do
   before do
-    club1 = Club.create!(
-    name: cl1name,
-    city: cl1city,
-    description: cl1description);
+    club1 = Club.create!(cl1attrs);
 
-    club2 = Club.create!(
-    name: cl2name,
-    city: cl2city,
-    description: cl2description);
+    club2 = Club.create!(cl2attrs);
   end
 
   scenario "view one Club" do
     visit "#/clubs"
     click_on cl1name
 
-    expect(page).to have_content(cl1name)
-    expect(page).to have_content(cl1city)
-    expect(page).to have_content(cl1description)
+    expect(page).to have_content(cl1attrs[:name])
+    expect(page).to have_content(cl1attrs[:city])
+    expect(page).to have_content(cl1attrs[:description])
 
     click_on "Index"
 
-    expect(page).to have_content(cl2name)
-    expect(page).to_not have_content(cl2city)
-    expect(page).to_not have_content(cl2description)
+    expect(page).to have_content(cl2attrs[:name])
+    expect(page).to_not have_content(cl2attrs[:city])
+    expect(page).to_not have_content(cl2attrs[:description])
 
   end
 end
