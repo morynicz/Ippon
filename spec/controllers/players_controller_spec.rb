dd2fd7c82a85201cb658e0533164258928cfc317
Michał Orynicz <morynicz@gmail.com>
2016-06-15 22:49:28 +0200

    Add PlayersController#update rb spec

--- spec/controllers/players_controller_spec.rb
+++ spec/controllers/players_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 RSpec.describe PlayersController, type: :controller do
   render_views
 
+  def authorize_user(club_id)
+    ClubAdmin.create(club_id: club_id, user_id: current_user.id)
+  end
+
   before(:each) do
     @request.env["devise.mapping"] = Devise.mappings[:user]
   end
@@ -134,6 +138,132 @@ RSpec.describe PlayersController, type: :controller do
       it "should respond with 404 status" do
         action
         expect(response.status).to eq(404)
+      end
+    end
+  end
+
+  describe "PATCH update" do
+    let(:club) {
+      FactoryGirl::create(:club)
+    }
+
+
+    let(:action) {
+      xhr :put, :update, format: :json, id: player.id, player: update_player_attrs
+      player.reload
+    }
+
+    let(:update_player_attrs) {
+      FactoryGirl::attributes_for(:player)
+    }
+    let(:player_attrs) {
+      FactoryGirl::attributes_for(:player, club_id: club.id)
+    }
+    context "when the player exists" do
+      let(:player) {
+        Player.create(player_attrs)
+      }
+
+      context "when the user is authorized", authenticated: true do
+        before do
+          authorize_user(club.id)
+        end
+        context "when the update atttributes are valid" do
+          it "should return correct status" do
+            action
+            expect(response.status).to eq(204)
+          end
+
+          it "should update player name" do
+            action
+            expect(player.name).to eq(update_player_attrs[:name])
+          end
+
+          it "should update player birthday" do
+            action
+            expect(player.birthday).to eq(update_player_attrs[:birthday])
+          end
+
+          it "should update player rank" do
+            action
+            expect(Player.ranks[player.rank]).to eq(update_player_attrs[:rank])
+          end
+
+          it "should update player surname" do
+            action
+            expect(player.surname).to eq(update_player_attrs[:surname])
+          end
+
+          it "should update player sex" do
+            action
+            expect(Player.sexes[player.sex]).to eq(update_player_attrs[:sex])
+          end
+
+          it "should update player club id" do
+            action
+            expect(player.club.id).to eq(update_player_attrs[:club_id])
+          end
+        end
+
+        context "when the update attributes are not valid" do
+          let(:update_player_attrs) {
+            {
+              name: '',
+              surname: '',
+              birthday: '',
+              rank: '',
+              sex: '',
+              club_id: ''
+            }
+          }
+
+          it "should not update player attributes" do
+            action
+            expect(player.name).to eq(player_attrs[:name])
+            expect(player.surname).to eq(player_attrs[:surname])
+            expect(player.birthday).to eq(player_attrs[:birthday])
+            expect(Player.sexes[player.sex]).to eq(player_attrs[:sex])
+            expect(Player.ranks[player.rank]).to eq(player_attrs[:rank])
+            expect(player.club.id).to eq(player_attrs[:club_id])
+          end
+
+          it "should return unporcessable entity" do
+            action
+            expect(response).to have_http_status :unprocessable_entity
+          end
+        end
+      end
+
+      context "when the user isn't authorized" do
+        it "should respond with unauthorized status" do
+          action
+          expect(response).to have_http_status :unauthorized
+        end
+
+        it "should not update player attributes" do
+          action
+          expect(player.name).to eq(player_attrs[:name])
+          expect(player.surname).to eq(player_attrs[:surname])
+          expect(player.birthday).to eq(player_attrs[:birthday])
+          expect(Player.sexes[player.sex]).to eq(player_attrs[:sex])
+          expect(Player.ranks[player.rank]).to eq(player_attrs[:rank])
+          expect(player.club.id).to eq(player_attrs[:club_id])
+        end
+      end
+    end
+
+    context "when the player doesn't exist" do
+      let(:player_id) {-9999}
+
+      let(:action) {
+        xhr :put, :update, format: :json, id: player_id, player: update_player_attrs
+      }
+
+      context "when user is not authorized" do
+        it "should respond with unauthorized status" do
+          action
+          expect(response).to have_http_status :unauthorized
+        end
       end
     end
   end
--- spec/factories/players.rb
+++ spec/factories/players.rb
@@ -5,6 +5,6 @@ FactoryGirl.define do
     birthday {Faker::Date.between(60.years.ago, 5.years.ago)}
     rank {Faker::Number.between(0,13)}
     sex {Faker::Number.between(0,1)}
-    club
+    club_id {FactoryGirl::create(:club).id}
   end
 end
