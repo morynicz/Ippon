324a067727488e82d26311f9b35c35759d7e24fe
Michał Orynicz <morynicz@gmail.com>
2016-06-15 07:01:38 +0200

    Add authorization helper function to Player model

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,16 @@
 class PlayersController < ApplicationController
 
+  def authorize_user
+    if user_signed_in?
+      user = current_user
+      player = find(params[:id])
+      club = player.club
+      head :unauthorized unless ClubAdmin.exists?(club_id: club.id, user_id: user.id)
+    else
+      head :unauthorized
+    end
+  end
+
   def index
     @players = Player.all
   end
