586e799d96db7af2ebafe729d2dc4524b7cbc63a
Michał Orynicz <morynicz@gmail.com>
2016-07-10 14:14:58 +0200

    Implement TournamentsController#show

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,2 +1,18 @@
 class Tournament < ActiveRecord::Base
+  enum state: {
+    setup: 0,
+    registration: 1,
+    verification: 2,
+    preparation: 3,
+    groups: 4,
+    playoff: 5,
+    closed: 6
+  }
+
+  enum constraint: {
+    no_constraint: 0,
+    less_or_equal: 1,
+    greater_or_equal: 2,
+    equal: 3
+  }
 end
