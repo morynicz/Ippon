7b1028e89e58c1fe475a7dc8af71feef3a0cc854
Michał Orynicz <morynicz@gmail.com>
2016-07-14 07:34:15 +0200

    Add team_with_p[layers factory to teams factories

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -3,5 +3,18 @@ FactoryGirl.define do
     name {Faker::Lorem.word}
     required_size 1
     tournament {FactoryGirl::create(:tournament)}
+
+    factory :team_with_players do
+      transient do
+        players_count 5
+      end
+
+      after(:create) do |team, evaluator|
+        players = create_list(:player, evaluator.players_count)
+        for player in players do
+          TeamMembership.create(player_id: player.id, team_id: team.id)
+        end
+      end
+    end
   end
 end
