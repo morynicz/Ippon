cc891dac84772e74c30cc2bc2d157e13c63d0398
Michał Orynicz <morynicz@gmail.com>
2016-08-27 10:33:15 +0200

    Define association betwean Team model and Player model

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,6 @@
 class Team < ActiveRecord::Base
   validates :name, :required_size, :tournament_id, presence: true
   belongs_to :tournament
+  has_many :team_memberships
+  has_many :players, through: :team_memberships
 end
