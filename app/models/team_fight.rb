01bbe26ac192623cbc4c4865e9d2dafe4d9bf08a
Michał Orynicz <morynicz@gmail.com>
2016-09-14 08:56:18 +0200

    Add aka and shiro members assoc to TeamFight

--- app/models/team_fight.rb
+++ app/models/team_fight.rb
@@ -1,4 +1,7 @@
 class TeamFight < ActiveRecord::Base
   belongs_to :shiro_team, class_name: "Team", foreign_key: "shiro_team_id"
   belongs_to :aka_team, class_name: "Team", foreign_key: "aka_team_id"
+
+  has_many :shiro_members, through: :shiro_team, source: :players
+  has_many :aka_members, through: :aka_team, source: :players
 end
