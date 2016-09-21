b161972ba3c5c0b6fc075d2308c19a086162ade2
Michał Orynicz <morynicz@gmail.com>
2016-09-21 15:09:51 +0200

    Dont use faker for generating player names and surnames
    Faker does not profuce unique pairs and breaks tests

--- spec/factories/players.rb
+++ spec/factories/players.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :player do
-    name {Faker::Name.first_name}
-    surname {Faker::Name.last_name}
+    sequence(:name) {|n| "Name#{n}"}
+    sequence(:surname) {|n| "Surname#{n}"}
     birthday {Faker::Date.between(60.years.ago, 5.years.ago)}
     rank {Player.ranks.invert[Faker::Number.between(0,13)]}
     sex {Player.sexes.invert[Faker::Number.between(0,1)]}
