971f3d00cc338fde517aa2f0223be05f883c6088
Michał Orynicz <morynicz@gmail.com>
2016-09-21 16:01:36 +0200

    Implement TeamFightsController#show spec

--- app/controllers/team_fights_controller.rb
+++ app/controllers/team_fights_controller.rb
@@ -1,2 +1,12 @@
 class TeamFightsController < ApplicationController
+  def show
+    @team_fight = TeamFight.find(params[:id])
+    @fights = @team_fight.fights
+    if user_signed_in? && @team_fight != nil
+      @isAdmin = TournamentAdmin.exists?(tournament_id: @team_fight.tournament
+        .id, user_id: current_user.id, status: TournamentAdmin.statuses[:main])
+    else
+      @isAdmin = false
+    end
+  end
 end
---
+++ app/views/team_fights/_team_fight.json.jbuilder
@@ -0,0 +1,2 @@
+json.(team_fight, :id, :aka_team_id, :aka_score, :shiro_team_id,
+  :shiro_score, :location_id, :state)
