0106a875ef1c1d5d6c211ef2a56a04b5582ecd76
Michał Orynicz <morynicz@gmail.com>
2016-09-21 13:55:45 +0200

    Add TeamFight#aka/shiro_score spec

--- spec/models/team_fight_spec.rb
+++ spec/models/team_fight_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe TeamFight, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "aka and shiro score" do
+    let(:tournament){FactoryGirl::create(:tournament, team_size: 3)}
+    let(:team_fight){
+      FactoryGirl::create(:team_fight_with_fights_and_points,
+        tournament: tournament, aka_points: 5, shiro_points: 3)}
+
+    context "when aka_score called" do
+      it "returns 5 points for aka" do
+        expect(team_fight.aka_score).to eq(5)
+      end
+    end
+
+    context "when shiro_score called" do
+      it "returns 3 points for shiro" do
+        expect(team_fight.shiro_score).to eq(3)
+      end
+    end
+
+  end
+
 end
