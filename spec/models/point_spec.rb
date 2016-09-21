e873968eec72409c8552a21ae1acba83a27999b3
Michał Orynicz <morynicz@gmail.com>
2016-09-21 13:10:11 +0200

    Add Point#value spec

--- spec/models/point_spec.rb
+++ spec/models/point_spec.rb
@@ -2,4 +2,41 @@ require 'rails_helper'
 
 RSpec.describe Point, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "value" do
+    context "point type is men" do
+      let(:point) { FactoryGirl::create(:point, type: :men)}
+      it "returns a hit value" do
+        expect(point.value).to eq(Point::HIT_VALUE)
+      end
+    end
+
+    context "point type is kote" do
+      let(:point) { FactoryGirl::create(:point, type: :kote)}
+      it "returns a hit value" do
+        expect(point.value).to eq(Point::HIT_VALUE)
+      end
+    end
+
+    context "point type is do" do
+      let(:point) { FactoryGirl::create(:point, type: :do)}
+      it "returns a hit value" do
+        expect(point.value).to eq(Point::HIT_VALUE)
+      end
+    end
+
+    context "point type is tsuki" do
+      let(:point) { FactoryGirl::create(:point, type: :tsuki)}
+      it "returns a hit value" do
+        expect(point.value).to eq(Point::HIT_VALUE)
+      end
+    end
+
+    context "point type is hansoku" do
+      let(:point) { FactoryGirl::create(:point, type: :hansoku)}
+      it "returns a hit value" do
+        expect(point.value).to eq(Point::HANSOKU_VALUE)
+      end
+    end
+  end
 end
