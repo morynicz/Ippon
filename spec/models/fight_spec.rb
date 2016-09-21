893dc8b2b904cbac578c36783d3e52cb23bee307
Michał Orynicz <morynicz@gmail.com>
2016-09-21 13:26:58 +0200

    Add Fight#score spec

--- spec/models/fight_spec.rb
+++ spec/models/fight_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Fight, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "player_score" do
+    let(:fight) {
+      f = FactoryGirl::create(:fight)
+      FactoryGirl::create(:point, fight: f, player: f.aka, type: :kote)
+      FactoryGirl::create(:point, fight: f, player: f.aka, type: :hansoku)
+
+      FactoryGirl::create(:point, fight: f, player: f.shiro, type: :tsuki)
+      FactoryGirl::create(:point, fight: f, player: f.shiro, type: :do)
+      return f
+    }
+
+    context "when called" do
+      it "returns correct score for shiro player (KOTE + HANSOKU)" do
+        expect(fight.shiro_score).to eq(Point::HIT_VALUE +
+          Point::HANSOKU_VALUE)
+      end
+
+      it "returns correct score for aka player (DO + TSUKI)" do
+        expect(fight.aka_score).to eq(Point::HIT_VALUE * 2)
+      end
+    end
+
+  end
 end
