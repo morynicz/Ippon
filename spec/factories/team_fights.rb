dea93f6556c47f1f881a072322f9448c200bffef
Michał Orynicz <morynicz@gmail.com>
2016-09-21 13:54:16 +0200

    Add team_fight_with_fights_adn_points factory

--- spec/factories/team_fights.rb
+++ spec/factories/team_fights.rb
@@ -3,5 +3,26 @@ FactoryGirl.define do
     shiro_team {create(:team)}
     aka_team {create(:team, tournament: shiro_team.tournament)}
     location {create(:location, tournament: shiro_team.tournament)}
+
+    factory :team_fight_with_fights_and_points do
+      transient do
+        shiro_points 2
+        aka_points 1
+      end
+
+      after(:create) do |team_fight, evaluator|
+        FactoryGirl::create_list(:fight, team_fight.tournament.team_size,
+          team_fight: team_fight)
+        evaluator.shiro_points.times {
+          fight = team_fight.fights.shuffle.first
+          FactoryGirl::create(:point, fight: fight, player: fight.aka)
+        }
+
+        evaluator.aka_points.times {
+          fight = team_fight.fights.shuffle.first
+          FactoryGirl::create(:point, fight: fight, player: fight.shiro)
+        }
+      end
+    end
   end
 end
