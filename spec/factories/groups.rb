a4953ee3dee758672d1893ea4fb95b2649d76df6
Michał Orynicz <morynicz@gmail.com>
2016-12-11 21:04:09 +0100

    Add new factories for groups
    Add group_with_members and group_with_fights
    factories.

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -2,5 +2,30 @@ FactoryGirl.define do
   factory :group do
     tournament
     sequence(:name, 'A') {|n| "Group #{n}"}
+
+    factory :group_with_members do
+      transient do
+        members_count 4
+      end
+
+      after(:create) do |group, evaluator|
+        members = create_list(:team, evaluator.members_count, tournament: group.tournament)
+
+        for member in members do
+          GroupMember.create(team_id: member.id, group_id: group.id)
+        end
+      end
+
+      factory :group_with_fights do
+        after(:create) do |group, evaluator|
+          members = group.group_members
+
+          members.combination(2) {|c|
+            tf = FactoryGirl::create(:team_fight, tournament: group.tournament, aka_team: c[0].team, shiro_team: c[1].team)
+            GroupFight.create(group_id: group.id, team_fight_id: tf.id)
+          }
+        end
+      end
+    end
   end
 end
