1f613a4a64859d2d6fea2130b3226d50adaff050
Michał Orynicz <morynicz@gmail.com>
2017-03-23 08:16:01 +0100

    Add GroupFight#delete spec
    Ensure that deletion also propagates to underlying team fight

--- spec/models/group_fight_spec.rb
+++ spec/models/group_fight_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe GroupFight, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    describe "delete" do
+      context "when the group fight is deleted" do
+        let(:group_fight) { FactoryGirl::create(:group_fight)}
+        it "deletes it's team fight" do
+          team_fight_id = group_fight.team_fight.id
+          group_fight.destroy
+
+          expect(TeamFight.exists?(team_fight_id)).to be false
+        end
+      end
+    end
 end
