defe5235ef123da0068bb95da09c471cb6fbc121
Michał Orynicz <morynicz@gmail.com>
2017-03-25 10:10:49 +0100

    Add tournament_createable shared examples

---
+++ spec/support/shared_examples/tournament_createable.rb
@@ -0,0 +1,77 @@
+shared_examples_for "tournament_createable" do
+  describe "POST :create" do
+
+    subject(:results) {JSON.parse(response.body)}
+
+    context "when the user is not authenticated" do
+      it "does not create a resource" do
+        expect {
+          action
+        }.to_not change(resource_class, :count)
+      end
+
+      it "denies access" do
+        action
+        expect(response).to have_http_status :unauthorized
+      end
+    end
+
+    context "when the user is authenticated", authenticated: true do
+      context "when user is authorized" do
+        before do
+          authorize_user(tournament.id)
+        end
+
+        context "with invalid attributes" do
+
+          let(:attributes){ bad_attributes }
+
+          it "does not create a resource" do
+            expect {
+              action
+            }.to_not change(resource_class, :count)
+          end
+
+          it "returns the correct status" do
+            action
+            expect(response).to have_http_status :unprocessable_entity
+          end
+        end
+
+        context "with valid attributes" do
+          it "creates a resource" do
+            expect {
+              action
+            }.to change(resource_class, :count).by(1)
+          end
+
+          it "returns the correct status" do
+            action
+            response.inspect
+            expect(response).to be_successful
+          end
+
+          it "creates a resource with proper values" do
+            action
+            res = resource_class.find(results["group_fight"]["id"])
+            expect_hash_eq_resource_create(attributes, res)
+          end
+        end
+      end
+
+      context "when the user is not authorized" do
+        it "should respond with unauthorized status" do
+          action
+          expect(response).to have_http_status :unauthorized
+        end
+
+        it "does not create a group fight" do
+          expect {
+            action
+          }.to_not change(resource_class, :count)
+        end
+      end
+    end
+  end
+end
+
