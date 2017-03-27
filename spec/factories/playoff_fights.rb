64c04474125911cb7c6604daa0a38617fc767d8f
Michał Orynicz <morynicz@gmail.com>
2017-03-27 21:52:13 +0200

    Improve playoff fights factory

--- spec/factories/playoff_fights.rb
+++ spec/factories/playoff_fights.rb
@@ -1,9 +1,16 @@
 FactoryGirl.define do
-  factory :playoff_fight do
+  factory :playoff_fight, aliases: [:previous_aka_fight, :previous_shiro_fight] do
     tournament
-    team_fight {build(:team_fight, tournament: tournament)}
+    team_fight {association :team_fight, tournament: tournament}
     previous_aka_fight nil
     previous_shiro_fight nil
     location {tournament.locations.shuffle.first}
+
+    trait :with_previous_fights do
+      previous_aka_fight {association :previous_aka_fight, tournament: tournament}
+      previous_shiro_fight {association :previous_shiro_fight, tournament: tournament}
+    end
+
+    factory :playoff_fight_with_previous_fights, traits: [:with_previous_fights]
   end
 end
