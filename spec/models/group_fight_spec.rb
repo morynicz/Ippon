7ffe4cfb17e5965989d87fd9c0622e82ba863c33
Michał Orynicz <morynicz@gmail.com>
2017-03-29 08:25:12 +0200

    Add PlayoffFight spec

--- spec/models/group_fight_spec.rb
+++ spec/models/group_fight_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe GroupFight, type: :model do
     describe "destroy" do
-      context "when the group fight is deleted" do
+      context "when the group fight is destroyed" do
         let(:group_fight) { FactoryGirl::create(:group_fight)}
         it "deletes it's team fight" do
           team_fight_id = group_fight.team_fight.id
--- spec/models/playoff_fight_spec.rb
+++ spec/models/playoff_fight_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe PlayoffFight, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when the playoff fight is deleted" do
+          let(:playoff_fight) { FactoryGirl::create(:playoff_fight)}
+          it "deletes it's team fight" do
+            team_fight_id = playoff_fight.team_fight.id
+            playoff_fight.destroy
+
+            expect(TeamFight.exists?(team_fight_id)).to be false
+          end
+        end
 end
