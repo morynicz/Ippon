3176848718f62a89a47acbe46295efaa1d47de9c
Michał Orynicz <morynicz@gmail.com>
2017-04-04 22:50:23 +0200

    Improve playpoff_fight factory

--- spec/factories/playoff_fights.rb
+++ spec/factories/playoff_fights.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :playoff_fight, aliases: [:previous_aka_fight, :previous_shiro_fight] do
     tournament
-    team_fight {association :team_fight, tournament: tournament}
+    team_fight nil
     previous_aka_fight nil
     previous_shiro_fight nil
 
@@ -10,6 +10,10 @@ FactoryGirl.define do
       previous_shiro_fight {association :previous_shiro_fight, tournament: tournament}
     end
 
+    trait :with_team_fight do
+      team_fight {association :team_fight, tournament: tournament}
+    end
     factory :playoff_fight_with_previous_fights, traits: [:with_previous_fights]
+    factory :playoff_fight_with_team_fight, traits: [:with_team_fight]
   end
 end
