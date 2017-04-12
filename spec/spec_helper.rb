dfe6efa80071d172d9e02617637ce8e9bf143341
Michał Orynicz <morynicz@gmail.com>
2017-04-12 08:13:54 +0200

    Remove factory girl linter. It's useless

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -76,7 +76,6 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.clean_with :truncation
     DatabaseCleaner.clean_with :transaction
-    FactoryGirl.lint
   end
 
   config.after(:each) do
