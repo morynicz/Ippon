41789f18731d2a7943866e1a949b4107a677b901
Michał Orynicz <morynicz@gmail.com>
2017-05-12 08:08:43 +0200

    Add tournament child viewing spec: view team details

---
+++ spec/features/tournaments/view_children_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper.rb'
+
+feature "Viewing children", js: true do
+  scenario "view all Teams" do
+    tournament = FactoryGirl::create(:tournament_with_all)
+    visit "#/tournaments"
+    click_on tournament[:name]
+
+    for team in tournament.teams
+      expect(page).to have_content(team[:name])
+    end
+  end
+
+  scenario "view Team details" do
+    tournament = FactoryGirl::create(:tournament_with_all)
+    visit "#/tournaments"
+    click_on tournament[:name]
+
+    team = tournament.teams.last
+    within('section#teams-list') do
+      click_on team[:name]
+    end
+    expect(page).to have_content(team[:name])
+    for player in team.players
+      expect(page).to have_content(player[:name])
+      expect(page).to have_content(player[:surname])
+      expect(page).to have_content(player[:club])
+    end
+  end
+
+  scenario "get back from Team details to Tournament" do
+    tournament = FactoryGirl::create(:tournament_with_all)
+    visit "#/tournaments"
+    click_on tournament[:name]
+    team = tournament.teams.last
+    within('section#teams-list') do
+      click_on team[:name]
+    end
+    click_on "back-team"
+    expect(page).to have_content(tournament[:name])
+  end
+end
